285571aa24fe7893add4066950097b6fb3083045
molodoj88 <molodoj88@gmail.com>
2017-12-21 17:20:13 +0300

    added table model/view

--- form.py
+++ form.py
@@ -1,20 +1,20 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtCore import Qt, QModelIndex
 from PyQt5.QtCore import QThread
 
 
-class MainWindow(QtWidgets.QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+
         self.init_ui()
 
     def init_ui(self):
         self.resize(800, 600)
         self.setWindowTitle('Sniffer')
         self.main_widget = QtWidgets.QWidget()
-        self.main_layout = QtWidgets.QHBoxLayout()
-        self.setLayout(self.main_layout)
-        self.main_layout.addWidget(self.main_widget)
+        self.setCentralWidget(self.main_widget)
         self.init_table(self.main_widget)
         self.show()
 
@@ -24,21 +24,82 @@ class MainWindow(QtWidgets.QWidget):
         self.table = QtWidgets.QTableView()
         header = self.table.horizontalHeader()
         header.setStretchLastSection(True)
+        header.setMinimumSectionSize(200)
         self.table.resizeColumnsToContents()
         self.table.setModel(self.model)
         layout.addWidget(self.table)
+
+    def init_toolbar(self):
+        self.addToolBar('Start')
 
 
 class MyModel(QtCore.QAbstractTableModel):
     def __init__(self, parent=None):
         super(MyModel, self).__init__(parent)
+        self.flows = []
+        self.columnNames = ['Source IP: port', 'Destination IP: port', 'Packet Count']
 
     def rowCount(self, parent=None, *args, **kwargs):
-        return 5
+        return len(self.flows)
 
     def columnCount(self, parent=None, *args, **kwargs):
-        return 4
+        return len(self.columnNames)
 
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return None
+
+        if 0 <= index.row() < len(self.flows):
+            return None
+
+        if role == Qt.DisplayRole:
+
+            for i in range(self.columnCount()):
+                if index.columns() == i:
+                    return self.flows[index.row()][self.columnNames[i]]
+
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role != Qt.DisplayRole:
+            return None
+
+        if orientation == Qt.Horizontal:
+            for i in range(self.columnCount()):
+                if section == i:
+                    return self.columnNames[i]
+
+        return None
+
+    def insertRows(self, pos=0, count=1, parent=None):
+        self.beginInsertRows(QModelIndex(), pos, pos + count - 1)
+
+        for row in range(count):
+            self.flows.insert(row + pos, {"1": "", "2": "", "3": "", "4": ""})
+
+        self.endInsertRows()
+        return True
+
+    def setData(self, index, value, role=Qt.EditRole):
+        if role != Qt.EditRole:
+            return False
+
+        if index.isValid() and 0 <= index.row() < len(self.flows):
+            flow = self.flows[index.row()]
+            ic = index.column()
+
+            if ic == 0:
+                flow["1"] = value
+            elif ic == 1:
+                flow["2"] = value
+            elif ic == 2:
+                flow["3"] = value
+            elif ic == 3:
+                flow["4"] = value
+
+            self.dataChanged.emit(index, index)
+            return True
+
+        return False
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
